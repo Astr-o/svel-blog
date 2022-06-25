9fd48ad080ee4cebe78228b361039035ae3c6d7f
Kroum Klutchkov <kroum.klutchkov@gmail.com>
2022-06-26 01:59:57 +1000

    adds some global styles

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,8 +2,36 @@
 	import Nav from '../lib/components/nav.svelte';
 </script>
 
-<main>
+<style global>
+	* { 
+		margin: auto;
+	}
+
+	h1 {
+		padding-bottom: 10px;
+	}
+
+	h2 {
+		padding-bottom: 8px;
+	}
+
+	p {
+		padding-bottom: 5px;
+	}
+
+	.content {
+		background-color: bisque;
+		padding: 20px;
+	}
+
+	.footer {
+		background-color: aquamarine;
+		padding: 20px;
+	}
+</style>
+
+<main class = "content">
 	<slot />
 </main>
 
-<footer><Nav /></footer>
+<footer class = "footer"><Nav /></footer>
