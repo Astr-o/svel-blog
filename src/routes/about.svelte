9fee86d9f39e0c8426c09dc2133a2962bd072630
Kroum Klutchkov <kroum.klutchkov@gmail.com>
2022-06-26 01:21:04 +1000

    adds some basic routes

---
+++ src/routes/about.svelte
@@ -0,0 +1,7 @@
+<script>
+	import Nav from '../components/nav.svelte';
+</script>
+
+<h1>About</h1>
+
+<Nav />
---
+++ src/routes/blog/index.svelte
@@ -0,0 +1,7 @@
+<script>
+	import Nav from '../../components/nav.svelte';
+</script>
+
+<h1>Blog</h1>
+
+<Nav />
