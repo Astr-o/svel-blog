4d85e5f33412e7a89e92b99d81e6d2b5eebf9214
Kroum Klutchkov <kroum.klutchkov@gmail.com>
2022-06-26 22:30:24 +1000

    adds basic home themeing

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -9,29 +9,5 @@
 <footer class="footer"><Nav /></footer>
 
 <style global>
-	* {
-		margin: auto;
-	}
 
-	h1 {
-		padding-bottom: 10px;
-	}
-
-	h2 {
-		padding-bottom: 8px;
-	}
-
-	p {
-		padding-bottom: 5px;
-	}
-
-	.content {
-		background-color: bisque;
-		padding: 20px;
-	}
-
-	.footer {
-		background-color: aquamarine;
-		padding: 20px;
-	}
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<header id="home">
+    <div class="row banner">
+        <div class="banner-text">
+            <h1 class="responsive-headline">I'm Kroum Klutchkov.</h1>
+            <h3>
+                I'm a Sydney Based <span>Technology Consultant</span>, <span>Principal Engineer</span> and
+                <span>Science Nerd</span>.
+                Enabling teams to deliver holistic and engaging digital experiences at scale in the cloud! 🚀☁
+            </h3>
+            <hr />
+            <ul class="social">
+                <li><a href="https://www.linkedin.com/in/kklutchkov/" alt="My LinkedIn"><i class="fa fa-linkedin" /></a></li>
+                <li><a href="https://github.com/Astr-o"><i class="fa fa-github" /></a></li>
+                <li><a href="https://stackoverflow.com/users/2907286/astr-o"><i class="fa fa-stack-overflow"/></a></li>
+            </ul>
+        </div>
+    </div>
+    
+    <!-- <p class="scrolldown">
+        <a class="smoothscroll" href="#about"><i class="icon-down-circle" /></a>
+    </p> -->
+</header>
+
+
