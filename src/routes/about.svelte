942ab23d3a5607de8a85c41c4c5be713cff70408
Kroum Klutchkov <kroum.klutchkov@gmail.com>
2022-06-26 23:44:12 +1000

    updated about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,1 +1,120 @@
-<h1>About</h1>
+<!-- About Section
+   ================================================== -->
+<section id="about">
+	<div class="row">
+		<div class="three columns">
+			<img class="profile-pic" src="images/profilepic.jpg" alt="" />
+		</div>
+
+		<div class="nine columns main-col">
+			<h2>About Me</h2>
+
+            <p>
+                I am a Technical Lead, with over 4 years’ experience leading high-performance multifunctional teams and over 10 years’ experience in Software Engineering & Data Science.
+            </p>
+
+            <p>
+                I have broad technical skills in Cloud Architecture, Full-Stack Web Development, Data Science, Serverless Event-Driven Architectures, API led design, Statistics, Computer Science and experience with implementing solutions in Google and AWS Cloud.
+            </p>
+
+            <p>
+                I am a servant leader who is dedicated to ensuring my team is well supported, productive and working in a collaborative way. While maintaining our focus on technical and operational excellence.
+            </p>
+
+			<div class="row">
+				<div class="columns download">
+					<p>
+						<a href="#" class="button"><i class="fa fa-download" />Download Resume</a>
+					</p>
+				</div>
+			</div>
+			<!-- end row -->
+		</div>
+		<!-- end .main-col -->
+	</div>
+</section>
+<!-- About Section End-->
+
+<!-- Resume Section
+ ================================================== -->
+<section id="resume">
+	<!-- Work
+    ----------------------------------------------- -->
+	<div class="row work">
+		<div class="three columns header-col">
+			<h1><span>Projects</span></h1>
+		</div>
+
+		<div class="nine columns main-col">
+			<div class="row item">
+				<div class="twelve columns">
+					<h3>Versent</h3>
+					<p class="info">
+						Principal Engineer - Football Australia Digital Transformation<span>&bull;</span>
+						<em class="date">March 2022 - Present</em>
+					</p>
+
+					<p>
+						As Principal Software Engineer I am leading the techincal delivery of the next
+						generation Football Australia Digital Platform. This multi year and multi million dollar
+						program delivers a unified registration and payments experie
+					</p>
+				</div>
+			</div>
+			<!-- item end -->
+
+			<div class="row item">
+				<div class="twelve columns">
+					<h3>Paytron</h3>
+					<p class="info">
+						Technical Lead - Data & APIs <span>&bull;</span>
+						<em class="date">March 2021 - February 2022</em>
+					</p>
+
+					<p>
+						This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
+						Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis
+						sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi
+						accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non
+						mauris vitae erat
+					</p>
+				</div>
+
+				<div class="twelve columns">
+					<h3>Employsure</h3>
+					<p class="info">
+						Engineering Team Lead <span>&bull;</span>
+						<em class="date">March 2021</em>
+					</p>
+				</div>
+			</div>
+			<!-- item end -->
+		</div>
+		<!-- main-col end -->
+	</div>
+	<!-- End Work -->
+
+	<!-- Education
+    ----------------------------------------------- -->
+	<div class="row education">
+		<div class="three columns header-col">
+			<h1><span>Education</span></h1>
+		</div>
+
+		<div class="nine columns main-col">
+			<div class="row item">
+				<div class="twelve columns">
+					<h3>Australian National University</h3>
+					<p class="info">
+						Bachlour Science - Computer Science & Mathematics <span>&bull;</span>
+						<em class="date">2011 - 2015</em>
+					</p>
+				</div>
+			</div>
+			<!-- item end -->
+		</div>
+		<!-- main-col end -->
+	</div>
+	<!-- End Education -->
+</section>
+<!-- Resume Section End-->
