a9d704d438d421b49d04ce55d7bbb9ef21a8f5e8
Astr-o <kroum.klutchkov@gmail.com>
2022-07-09 10:44:17 +0000

    Deploying to gh-pages from @ Astr-o/svel-blog@13f511a2103c35677cedf3d621acb2d41ced3ded 🚀

---
+++ _app/immutable/pages/blog/my-first-post.md-4400ca08.js
@@ -0,0 +1,1 @@
+import"../../chunks/index-f5c4685a.js";import"../../chunks/blog-post-layout-45292c51.js";import{M as r,m as e}from"../../chunks/my-first-post-4b72f8d6.js";export{r as default,e as metadata};
---
+++ _app/immutable/pages/blog/my-second-post.md-8755f34f.js
@@ -0,0 +1,1 @@
+import"../../chunks/index-f5c4685a.js";import"../../chunks/blog-post-layout-45292c51.js";import{M as r,m as e}from"../../chunks/my-second-post-50e5c7ab.js";export{r as default,e as metadata};
